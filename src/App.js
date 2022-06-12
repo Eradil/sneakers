6040c96a3a83a20a890f1be5dc1fc068465a2bd6
Eradil <eradil.abasbekov.ss@gmail.comgit config --global user.name Eradil git config --global user.name Eradil git config --global user.email eradil.abasbekov.ss@gmail.com>
2022-06-12 17:40:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/media/logo.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React sneakers</h3>
+            <p>sneakers shop</p>
+          </div>
+        </div>
+        <ul className="headerRigth d-flex align-center">
+          <li className="mr-30 d-flex align-center">
+            <img width={18} height={18} src="/media/cart.svg" alt="logo" />
+            <span>1200$</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/media/heart.svg" alt="logo" />
+          </li>
+          <li>
+            <img width={18} height={18} src="/media/user.svg" alt="logo" />
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>
+
+        <div className="content_inner">
+          <div className="card mb-40">
+            <img
+              width={133}
+              height={112}
+              src="/media/sneakers/1.jpg"
+              alt="card-img"
+            />
+            <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
+            <div className="cardBottom  d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>price:</span>
+                <b>12000$</b>
+              </div>
+              <button className="btn">
+                <img width={32} height={32} src="/media/plus.svg" alt="plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card mb-40">
+            <img
+              width={133}
+              height={112}
+              src="/media/sneakers/2.jpg"
+              alt="card-img"
+            />
+            <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
+            <div className="cardBottom  d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>price:</span>
+                <b>12000$</b>
+              </div>
+              <button className="btn">
+                <img width={32} height={32} src="/media/plus.svg" alt="plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card mb-40">
+            <img
+              width={133}
+              height={112}
+              src="/media/sneakers/3.jpg"
+              alt="card-img"
+            />
+            <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
+            <div className="cardBottom  d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>price:</span>
+                <b>12000$</b>
+              </div>
+              <button className="btn">
+                <img width={32} height={32} src="/media/plus.svg" alt="plus" />
+              </button>
+            </div>
+          </div>
+          <div className="card mb-40">
+            <img
+              width={133}
+              height={112}
+              src="/media/sneakers/4.jpg"
+              alt="card-img"
+            />
+            <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
+            <div className="cardBottom  d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>price:</span>
+                <b>12000$</b>
+              </div>
+              <button className="btn">
+                <img width={32} height={32} src="/media/plus.svg" alt="plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
